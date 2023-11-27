f0952dbcb9d745a8fa80883abf74e7f45e3acd17
Simone <simone.dangelo636@gmail.com>
2023-11-27 15:09:26 +0100

    Spring initializer

---
+++ src/main/java/simonedangelo/mondovan/MondoVanApplication.java
@@ -0,0 +1,13 @@
+package simonedangelo.mondovan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MondoVanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MondoVanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/simonedangelo/mondovan/MondoVanApplicationTests.java
@@ -0,0 +1,13 @@
+package simonedangelo.mondovan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MondoVanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
